fd5973d5d115b081cf8d4a3f0d48a5fb658a82e1
Young <147588@naver.com>
2022-02-20 22:03:03 -0500

    Programming Exercise 11.12

---
+++ young/src/pgExcercise11_12/ex11_12.java
@@ -0,0 +1,33 @@
+package pgExcercise11_12;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ex11_12 {
+
+  public static void main(String[] args) {
+    // Main Method
+
+    // Creating Scanner
+    Scanner input = new Scanner(System.in);
+    // Ask user to input
+    System.out.print("Enter 5 numbers: ");
+    // Creating array
+    ArrayList<Double> list = new ArrayList<>();
+
+    for (int i = 0; i < 5; i++) {
+      list.add(input.nextDouble());
+    }
+    // Display the sum of the list
+    System.out.println("Sum of inputs are: " + sum(list));
+  }
+
+  // Return the numbers to ArrayList
+  public static double sum(ArrayList<Double> list) {
+    double sum = 0;
+    for (double i : list) {
+      sum += i;
+    }
+    return sum;
+  }
+}
