62907a70b51f08d4f3129e48c2463d590a8d3bb6
Young <147588@naver.com>
2022-03-27 21:02:05 -0400

    pgExercise13_7

---
+++ young/src/pgExercise13_7/Colorable.java
@@ -0,0 +1,6 @@
+package pgExercise13_7;
+
+public interface Colorable {
+  /** Describing how to color */
+  String howToColor();
+}
---
+++ young/src/pgExercise13_7/test.java
@@ -0,0 +1,12 @@
+package pgExercise13_7;
+
+import org.junit.jupiter.api.Test;
+
+class test {
+
+  @Test
+  void test() {
+
+  }
+
+}
